b2f4faced19d90d929ca1a76abb55205413b5456
herypank <60302361+herypank@users.noreply.github.com>
2020-08-04 12:29:31 +0300

    Update request.py

--- request.py
+++ request.py
@@ -38,9 +38,11 @@ def if_bitlink(bitly_token,user_url):
 
     AUTH_HEADERS = { 'Authorization' : f"Bearer {bitly_token}" }
 
+
     if user_url.find("://") != -1:
         urlib_parce = urlparse(user_url)
-        user_url = urlib_parce.geturl(scheme='')
+        urlib_parce = urlib_parce._replace(scheme='')
+        user_url = urlib_parce.geturl()[2:]
 
     link = 'https://api-ssl.bitly.com/v4/expand'
 
@@ -58,9 +60,12 @@ def count_clicks(bitly_token,user_url):
     возвращаемое значение количество кликов по битлинку'''
 
     AUTH_HEADERS = { 'Authorization' : f"Bearer {bitly_token}" }
-    if user_url.find("://") != -1:
-        user_url= user_url.split("://")[1]
 
+    if user_url.find("://") != -1:
+        urlib_parce = urlparse(user_url)
+        urlib_parce = urlib_parce._replace(scheme='')
+        user_url = urlib_parce.geturl()[2:]
+    
     link = f'https://api-ssl.bitly.com/v4/bitlinks/{user_url}/clicks/summary'
     payload = {"unit":'week', 'units': -1}
 
@@ -77,20 +82,22 @@ if __name__ == '__main__':
     args = parser.parse_args()
     user_url = args.url
 
+
     load_dotenv()
     bitly_token = os.getenv('BITLY_TOKEN')
-    
+    shorten_url = shorten_link(bitly_token,user_url)
+    counter_link = count_clicks(bitly_token,user_url)
     if if_bitlink(bitly_token,user_url):
         try: 
             count_clicks(bitly_token,user_url)
         except requests.exceptions.HTTPError:
             print('ОШИБКА. Ваша biy.ly ссылка не корректная!\nВведите ссылку в формате "bit.ly/30iqvat".')
         else:
-            print(f'Количество переходов по вашей ссылке {user_url}: {count_clicks(bitly_token,user_url)}')
+            print(f'Количество переходов по вашей ссылке {user_url}: {counter_link}')
     else:
         try: 
             shorten_link(bitly_token,user_url)
         except requests.exceptions.HTTPError:
             print('ОШИБКА. Ваша ссылка не корректная!\nВведите ссылку в формате "https://dvmn.org/modules/"')
         else:
-            print(f'Ваш битлинк: {shorten_link(bitly_token,user_url)}' )
+            print(f'Ваш битлинк: {shorten_url}' )
